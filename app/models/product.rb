8aa42aad34abcf1f24acf6db28a878e029e8ce50
prakashchandra <prakash.penta@careerbuilder.com>
2013-10-11 17:08:28 +0530

    28

--- app/models/product.rb
+++ app/models/product.rb
@@ -13,7 +13,7 @@ class Product < ActiveRecord::Base
 
   def self.search(query)
     # where(:title, query) -> This would return an exact match of the query
-    where("title like ?", "%#{query}%") 
+    where("title iLIKE ?", "%#{query}%") 
   end
   
 end
